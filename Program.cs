716f4d228f3db8dad8b819823a8a82915ce09cf0
Vocal <vocalnight@protonmail.com>
2023-11-16 00:14:21 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using HtmlAgilityPack;
+
+Console.WriteLine("Loading!");
+
+
+var url = "https://www.basketball-reference.com/boxscores/";
+var web = new HtmlWeb();
+var doc = web.Load(url);
+
+var title = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/h1").First().InnerText;
+
+var loserName = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div[1]/table[1]/tbody/tr[1]/td[1]/a").First().InnerText;
+var loserScore = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/h1").First().InnerText;
+
+var winnerName = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/h1").First().InnerText;
+var winnerScore = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/h1").First().InnerText;
+
+Console.WriteLine(loserName);
